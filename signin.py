c92bd1f57d2c3750feac8555e46bf9106d7bf904
go-daniel <pham.daniel.goldenowl@gmail.com>
2023-08-29 10:14:32 +0700

    merge

--- signin.py
+++ signin.py
@@ -1,12 +1,9 @@
 print("signin feature")
 
-<<<<<<< HEAD
 def signin():
     print("signin feature")
 
 def signinGoogle():
     print("signin feature with google")
 #add before rebase
-=======
 # add before git rebase
->>>>>>> 65f2dfccd36eda709b8f09bcdcedd431f9ab5214
